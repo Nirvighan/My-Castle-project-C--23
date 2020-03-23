9d21f54466ff90c3e1b9e3ac0b89bf6c3d947885
Nirvighan <62175651+Nirvighan@users.noreply.github.com>
2020-03-23 18:02:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+var sun,mercury,venus,earth,mars,jupiter,saturn,uranus,neptune;
+
+
+
+function setup() {
+  createCanvas(800,800);var sun = c
+
+  
+ 
+}
+
+function draw() {
+  background(220);
+  
+  
+    
+  fill("yellow");
+  ellipse(300,300,100,100);
+  
+  fill("grey");
+  ellipse(350,350,10,10);
+  
+  fill("orange");
+  ellipse(275,375,25,25);
+  
+  fill("green");
+  ellipse(200,420,35,35);
+  
+  fill("red");
+  ellipse(300,460,15);
+  
+  fill("brown");
+  ellipse(100,500,55,55);
+  
+  
+  fill("lightyellow");
+  ellipse(150,560,50,50);
+  
+  
+  fill("blue");
+  ellipse(250,610,40,40);
+  
+
+  fill("lightblue");
+  ellipse(275,660,38,38);
+  
+  
+ 
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+}
